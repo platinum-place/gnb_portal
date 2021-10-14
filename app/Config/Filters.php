77c052948aafb5328a0db66ceaceda189bceddea
platinumplace <warlyn200@gmail.com>
2021-10-14 08:01:31 -0400

    actualización para plantillas de casos

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -33,7 +33,7 @@ class Filters extends BaseConfig
         'before' => [
             // 'honeypot',
             // 'csrf',
-            'sesion' => ['except' => ['casos/*', 'login']],
+            'sesion' => ['except' => ['plantillas/*', 'login']],
         ],
         'after' => [
             //el toolbar ralentiza la carga cuando se listan grandes cantidades de registros
